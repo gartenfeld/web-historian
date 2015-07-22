267a1fc415050370edf075f238132376da2f7965
Hack Reactor Students <accounts@hackreactor.com>
2015-07-22 12:36:39 -0700

    return index or 404

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,37 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  var fetch = function (filePath, res) {
+    var absPath = path.resolve(__dirname, filePath);
+    fs.readFile(absPath, function(err, data) {
+      if (err) {
+        res.statusCode = 404;
+        res.end("Oops.");
+      } else {
+        res.end(data);
+      }
+    });
+  };
+
+  var ext = path.extname(req.url)
+  if (ext === '.html' || ext === '.css') {
+    console.log("Fetching: " + req.url);
+    fetch('./public/' + req.url, res);
+  } else if (req.url === '/') {
+    fetch('./public/index.html', res);
+  } else if (req.method === "GET") {
+    var siteName = req.url.slice(1);
+    fetch('../archives/sites/' + siteName, res)
+  } else if (req.method === "POST") {
+    console.log ("post :" + ext);
+    // fs.write('../archives/sites.text');
+  }
+
+
+    
+  
 };
